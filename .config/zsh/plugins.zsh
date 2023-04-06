8ec30f64182a1f4652e7bbac9b208eab385321af
portus-zephyr <66728045+portus-zephyr@users.noreply.github.com>
2023-04-06 22:27:16 +0800

    fix zinit intstall
    *pls test first*

--- .config/zsh/plugins.zsh
+++ .config/zsh/plugins.zsh
@@ -1,16 +1,16 @@
 # Install Zinit
 ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
 
-if [[ ! -f ${ZINIT_HOME}/zinit.git/zinit.zsh ]]; then
+if [[ ! -f ${ZINIT_HOME}/zinit.zsh ]]; then
     print -P "%F{14}▓▒░ Installing Flexible and fast ZSH plugin manager %F{13}(zinit)%f"
     command mkdir -p "${ZINIT_HOME}" && command chmod g-rwX "${ZINIT_HOME}"
-    command git clone https://github.com/zdharma-continuum/zinit.git "${ZINIT_HOME}/zinit.git" && \
+    command git clone https://github.com/zdharma-continuum/zinit.git "${ZINIT_HOME}" && \
         print -P "%F{10}▓▒░ Installation successful.%f%b" || \
         print -P "%F{9}▓▒░ The clone has failed.%f%b"
 fi
 
 # Enable Zinit completions
-source $ZINIT[BIN_DIR]/zinit.zsh \
+source "${ZINIT_HOME}/zinit.zsh" \
   && autoload -Uz _zinit \
   && (( ${+_comps} )) \
   && _comps[zinit]=_zinit
@@ -47,8 +47,6 @@ zinit wait lucid for \
     OMZL::grep.zsh \
     OMZL::key-bindings.zsh \
     OMZL::spectrum.zsh \
-    OMZL::termsupport.zsh \
-        atload"alias gcd='gco dev'" \
     OMZP::git \
     OMZP::fzf \
         atload"alias dcupb='docker-compose up --build'" \
