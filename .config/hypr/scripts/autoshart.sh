0655177c1e52217c5ec4c88d2c8c485480109729
portus-zephyr <66728045+portus-zephyr@users.noreply.github.com>
2023-04-07 00:35:53 +0800

    add autostart

---
+++ .config/hypr/scripts/autoshart.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sleep 4
+killall xdg-desktop-portal-wlr
+killall xdg-desktop-portal
+/usr/lib/xdg-desktop-portal-wlr &
+sleep 4
+/usr/lib/xdg-desktop-portal &
+
+# see https://wiki.hyprland.org/FAQ/#some-of-my-apps-take-a-really-long-time-to-open
