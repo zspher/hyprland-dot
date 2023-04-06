1de45d77c915b16b439da980feba88e0e7f62b01
portus-zephyr <66728045+portus-zephyr@users.noreply.github.com>
2023-04-07 00:50:54 +0800

    add keybinds

---
+++ .config/hypr/scripts/toggle_tile_layout.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+layout=$(hyprctl getoptions general:layout -j | jq '.str' -r)
+if [[ $layout = "master" ]]; then
+    hyprctl keyword general:layout dwindle
+elif [[ $layout = "dwindle " ]]; then
+    hyprctl keyword general:layout master
+fi
