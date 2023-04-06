6e394db4fd4f7b6ea9b3cb78976f42938d86181d
portus-zephyr <66728045+portus-zephyr@users.noreply.github.com>
2023-04-07 01:02:40 +0800

    fix toggle tile layout

--- .config/hypr/scripts/toggle_tile_layout.sh
+++ .config/hypr/scripts/toggle_tile_layout.sh
@@ -3,6 +3,6 @@
 layout=$(hyprctl getoptions general:layout -j | jq '.str' -r)
 if [[ $layout = "master" ]]; then
     hyprctl keyword general:layout dwindle
-elif [[ $layout = "dwindle " ]]; then
+elif [[ $layout = "dwindle" ]]; then
     hyprctl keyword general:layout master
 fi
