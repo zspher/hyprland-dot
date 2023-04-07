0cf4655d924d9103d2e3613160bf99593cc2183d
portus-zephyr <66728045+portus-zephyr@users.noreply.github.com>
2023-04-07 21:15:05 +0800

    chore: organize environment variable

--- .config/zsh/env.zsh
+++ .config/zsh/env.zsh
@@ -21,22 +21,23 @@ export PLATFORMIO_CORE_DIR="$XDG_DATA_HOME"/platformio
 export RANDFILE="$XDG_CONFIG_HOME/openssl/rnd"
 export RUSTUP_HOME="$XDG_DATA_HOME"/rustup
 export SSB_HOME="$XDG_DATA_HOME"/zoom
+export TEXMFCONFIG=$XDG_CONFIG_HOME/texlive/texmf-config
 export TEXMFHOME=$XDG_DATA_HOME/texmf
 export TEXMFVAR=$XDG_CACHE_HOME/texlive/texmf-var
-export TEXMFCONFIG=$XDG_CONFIG_HOME/texlive/texmf-config
 export VAGRANT_HOME="$XDG_DATA_HOME"/vagrant
 export WINEPREFIX="$XDG_DATA_HOME"/wine
 export WAKATIME_HOME="$XDG_CONFIG_HOME"/wakatime
-export WINEPREFIX="$XDG_DATA_HOME/wine"
 export XCURSOR_PATH=/usr/share/icons:${XDG_DATA_HOME}/icons
-
-export EDITOR="nvim"
-export VISUAL="nvim"
-export PAGER="bat -p"
 
 alias irssi='irssi --config="$XDG_CONFIG_HOME/irssi/config" --home="$XDG_DATA_HOME/irssi"'
 alias nvidia-settings='nvidia-settings --config="$XDG_CONFIG_HOME/nvidia/settings"'
 alias wget='wget --hsts-file="$XDG_DATA_HOME/wget-hsts"'
+
+# extra
+export EDITOR="nvim"
+export VISUAL="nvim"
+export PAGER="bat -p"
+export FZF_DEFAULT_COMMAND="find . -path '*/\.*' -type d -prune -o -type f -print -o -type l -print 2> /dev/null | sed s/^..//"
 
 # theme
 export BAT_THEME="Catppuccin-mocha"
@@ -52,5 +53,3 @@ export LESS_TERMCAP_se=$'\e[0m'
 export LESS_TERMCAP_so=$'\e[01;33m'
 export LESS_TERMCAP_ue=$'\e[0m'
 export LESS_TERMCAP_us=$'\e[1;4;31m'
-
-export FZF_DEFAULT_COMMAND="find . -path '*/\.*' -type d -prune -o -type f -print -o -type l -print 2> /dev/null | sed s/^..//"
