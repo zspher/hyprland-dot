b5f3b7c56efae0ad79b4bf6ea175e36d9c84ca5d
portus-zephyr <66728045+portus-zephyr@users.noreply.github.com>
2023-04-11 15:05:57 +0800

    update install script

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -5,9 +5,60 @@ if ! [ "$SHELL" = "$(which zsh)" ]; then
     sudo chsh -s "$(which zsh)" "$USER"
 fi
 
-cp .config/* ~/.config -r
-cp .local/bin ~/.local/bin/wrappedhl -r
-cp .zshenv ~/.zshenv
+copy_configs (){
+    cp .config/* ~/.config -r
+    cp .local/* ~/.local/ -r
+    cp .zshenv ~/.zshenv
+}
 
-git clone https://github.com/AstroNvim/AstroNvim ~/.config/nvim
+mkfile() { 
+    mkdir -p -- "$1" && touch -- "$1"/"$2" 
+}
+
+XDG_CONFIG_HOME="$HOME/.config"
+XDG_DATA_HOME="$HOME/.local/share"
+XDG_STATE_HOME="$HOME/.local/state"
+XDG_CACHE_HOME="$HOME/.cache"
+
+mk_xdg_dirs() {
+    mkdir -p "$XDG_CONFIG_HOME"/java
+    mkfile "$XDG_DATA_HOME" "z"
+    mkdir -p "$XDG_DATA_HOME"/cargo
+    mkdir -p "$XDG_CACHE_HOME"/nv
+    mkfile "$XDG_CONFIG_HOME"/git "config"
+    mkdir -p "$XDG_DATA_HOME"/gnupg
+    # chown -R $(whoami) "$XDG_DATA_HOME"/gnupg
+    # chmod 600 "$XDG_DATA_HOME"/gnupg/*
+    # chmod 700 "$XDG_DATA_HOME"/gnupg
+    mkdir -p "$XDG_DATA_HOME"/go
+    mkdir -p "$XDG_DATA_HOME"/gradle
+    mkfile "$XDG_CONFIG_HOME"/gtk-2.0 "gtkrc"
+    mkfile "$XDG_STATE_HOME"/zsh "history"
+    mkdir -p  "$XDG_CONFIG_HOME"/ipython
+    mkdir -p "$XDG_CONFIG_HOME"/kde
+    mkfile "$XDG_CACHE_HOME"/less "history"
+    mkfile "$XDG_DATA_HOME" "node_repl_history"
+    mkfile "$XDG_CONFIG_HOME"/npm "nmpmrc"
+    mkdir -p "$XDG_DATA_HOME"/platformio
+    mkfile "$XDG_CONFIG_HOME"/openssl "rnd"
+    mkdir -p "$XDG_DATA_HOME"/rustup
+    # mkdir -p "$XDG_DATA_HOME"/zoom
+    mkdir -p "$XDG_CONFIG_HOME"/texlive/texmf-config
+    mkdir -p "$XDG_DATA_HOME"/texmf
+    mkdir -p "$XDG_CACHE_HOME"/texlive/texmf-var
+    mkdir -p "$XDG_CONFIG_HOME"/vagrant
+    mkdir -p "$XDG_DATA_HOME"/wine
+    mkdir -p "$XDG_CONFIG_HOME"/wakatime
+    mkdir -p "$XDG_DATA_HOME"/icons
+
+    mkfile "$XDG_CONFIG_HOME"/irssi "config"
+    mkdir -p "$XDG_DATA_HOME"/irssi
+    mkfile "$XDG_DATA_HOME" "wget-hsts"
+    mkfile "$XDG_CONFIG_HOME"/nvidia "settings"
+}
+
+copy_configs
+mk_xdg_dirs
+
+# git clone https://github.com/AstroNvim/AstroNvim ~/.config/nvim
 # git clone https://github.com/portus-zephyr/astrovim_conf ~/.config/nvim/lua/user
