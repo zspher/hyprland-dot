5883a19e4313b6dc0290c1122b3b56cbf0e84b32
portus-zephyr <66728045+portus-zephyr@users.noreply.github.com>
2023-05-14 21:45:51 +0800

    feat: add keepassxc config

--- scripts/load-config.sh
+++ scripts/load-config.sh
@@ -4,3 +4,4 @@ rsync -azvhP ~/ .. --filter="merge ./copy.filter"
 
 ## redaction measures
 sed -i -e "/.*\[\$e\].*/d" -e "/.*screen:.*/d" -e "/^State.*/d" ../.config/okularrc
+sed -i -e '/^Own=.*/d' -e '/\[PasswordGenerator\]/Q' ../.config/keepassxc/keepassxc.ini
