1e49a9b8560126905c929adf7fce433fa9d78ac7
portus-zephyr <66728045+portus-zephyr@users.noreply.github.com>
2023-07-14 14:38:14 +0800

    fix: change Catppuccin gtk name to proper
    - recent changes in catppiccin/gtk have change all theme names from **-Dark** to **drak**

--- .config/zsh/env.zsh
+++ .config/zsh/env.zsh
@@ -48,7 +48,7 @@ export FZF_DEFAULT_OPTS=" \
 --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
 --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
-export GTK_THEME="Catppuccin-Mocha-Standard-Mauve-Dark"
+export GTK_THEME="Catppuccin-Mocha-Standard-Mauve-dark"
 export GREP_COLORS=${GREP_COLORS:-"mt=38;2;24;24;37;48;2;126;201;216"}
 export LS_COLORS="$(vivid generate catppuccin-mocha)"
 
