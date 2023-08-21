0f64ea35e6511bd41330ae78a9d047fc069b46f0
portus-zephyr <66728045+portus-zephyr@users.noreply.github.com>
2023-08-21 13:05:26 +0800

    scripts: moved `copy.filer` to util

--- scripts/load-config.sh
+++ scripts/load-config.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-rsync -azvhP ~/ .. --filter="merge ./copy.filter"
+rsync -azvhP ~/ .. --filter="merge ./util/copy.filter"
 
 ## redaction measures
 sed -i -e "/.*\[\$e\].*/d" -e "/.*screen:.*/d" -e "/^State.*/d" ../.config/okularrc
