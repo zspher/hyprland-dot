d82f2f10450ddc3b75f1aea91180a03277f1b4e8
portus-zephyr <66728045+portus-zephyr@users.noreply.github.com>
2023-08-27 17:26:44 +0800

    chore: update some minor configs

--- scripts/load-config.sh
+++ scripts/load-config.sh
@@ -5,3 +5,14 @@ rsync -azvhP ~/ .. --filter="merge ./util/copy.filter"
 ## redaction measures
 sed -i -e "/.*\[\$e\].*/d" -e "/.*screen:.*/d" -e "/^State.*/d" ../.config/okularrc
 sed -i -e '/^Own=.*/d' -e '/\[PasswordGenerator\]/Q' ../.config/keepassxc/keepassxc.ini
+
+
+files=(
+        ../.config/btop/btop.conf
+        ../.config/gtk-2.0/gtkrc
+        ../.config/qBittorrent/qBittorrent.conf
+        ../.config/qt5ct/qt5ct.conf
+)
+for f in "${files[@]}"; do
+    sed -i "s/$USER/\$USER/g" "$f"
+done
