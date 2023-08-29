ec447e889c2b489977fa52a4120b56efc896db02
portus-zephyr <66728045+portus-zephyr@users.noreply.github.com>
2023-08-29 12:22:00 +0800

    script: allow relative running

--- scripts/load-config.sh
+++ scripts/load-config.sh
@@ -1,18 +1,24 @@
 #!/usr/bin/env bash
 
-rsync -azvhP ~/ .. --filter="merge ./util/copy.filter"
+set -a
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+set +a
+
+echo "$SCRIPT_DIR"
+
+rsync -azvhP ~/ $SCRIPT_DIR/.. --filter="merge $SCRIPT_DIR/util/copy.filter"
 
 ## redaction measures
-sed -i -e "/.*\[\$e\].*/d" -e "/.*screen:.*/d" -e "/^State.*/d" ../.config/okularrc
-sed -i -e '/^Own=.*/d' -e '/\[PasswordGenerator\]/Q' ../.config/keepassxc/keepassxc.ini
+sed -i -e "/.*\[\$e\].*/d" -e "/.*screen:.*/d" -e "/^State.*/d" $SCRIPT_DIR/../.config/okularrc
+sed -i -e '/^Own=.*/d' -e '/\[PasswordGenerator\]/Q' $SCRIPT_DIR/../.config/keepassxc/keepassxc.ini
 
 
 files=(
-        ../.config/btop/btop.conf
-        ../.config/gtk-2.0/gtkrc
-        ../.config/qBittorrent/qBittorrent.conf
-        ../.config/qt5ct/qt5ct.conf
+    .config/btop/btop.conf
+    .config/gtk-2.0/gtkrc
+    .config/qBittorrent/qBittorrent.conf
+    .config/qt5ct/qt5ct.conf
 )
 for f in "${files[@]}"; do
-    sed -i "s/$USER/\$USER/g" "$f"
+    sed -i "s/$USER/\$USER/g" "$SCRIPT_DIR/../$f"
 done
