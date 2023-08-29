6cc0cd5bf89feb972aa5122e58edc0c679660e3c
portus-zephyr <66728045+portus-zephyr@users.noreply.github.com>
2023-08-29 11:56:52 +0800

    fix: install script location

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ SCRIPTS_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd 
 set +a
 
 mkdir -p log
-( bash "$SCRIPTS_DIR/scripts/01-configure.sh" )|& tee log/configure.log
+( bash "$SCRIPT_DIR/scripts/01-configure.sh" )|& tee log/configure.log
 ( bash "$SCRIPT_DIR/scripts/02-install-pkg.sh" )|& tee log/install.log
 ( bash "$SCRIPT_DIR/scripts/03-post-install.sh" )|& tee log/post-install.log
